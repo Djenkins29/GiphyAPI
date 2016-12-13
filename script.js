3ba9b6e521eb4b59bbca64b9c1573f663fa1cf03
djenkins29 <teamdave29@gmail.org>
2016-12-13 23:02:57 +0000

    gifs

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
-    
+
+
+   var search = $("#search").click(function() {
+   var input = $("#input").val();
+   console.log(input);
+     
      $.getJSON(
      
-      "https://api.giphy.com/v1/gifs/search?q=cars&api_key=dc6zaTOxFJmzC",
+      "https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=dc6zaTOxFJmzC",
      
      
-      function(response) {
+         
+      function (response) {
         console.log(response);
+        for(i=1 ; i<6 ; i++)
         $("#gif").append("<img src=" + response.data[0].images.fixed_width_downsampled.url + ">");
       });
-
-
-    $("#search").click(function() {
+     
+      
+//    var reset = $("#reset").click(function() {
+//    $(response).remove()
+//   }); 
    
-   var input = $("#input").val();
+   
+   
+   
 
-    
-    });   
+       
+       
 });
